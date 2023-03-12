d573e11c5cf6e1e5f206787dadd9aa605907498c
Harshv00 <harsh.8760@gmail.com>
2023-03-12 23:32:44 +0530

    update

---
+++ app/routes.py
@@ -0,0 +1,128 @@
+from flask import render_template,redirect,request,flash,session,url_for
+from flask_login import logout_user,current_user, login_user, login_required
+import cv2
+import mediapipe as mp
+import time
+from app import app,db
+from app.models import User
+from datetime import datetime
+from werkzeug.utils import secure_filename
+from joblib import load
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template('index.html',title='home')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username and password:
+            user = User.query.filter_by(username=username).first()
+            if user is None or not user.check_password(password):
+                flash('Invalid username or password','danger')
+                return redirect(url_for('login'))
+            login_user(user, remember=True)
+            return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In')
+
+    
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    if request.method=='POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        cpassword = request.form.get('cpassword')
+        password = request.form.get('password')
+        # print(cpassword, password, cpassword==password)
+        if username and password and cpassword and email:
+            if cpassword != password:
+                flash('Password do not match','danger')
+                return redirect('/register')
+            else:
+                if User.query.filter_by(email=email).first() is not None:
+                    flash('Please use a different email address','danger')
+                    return redirect('/register')
+                elif User.query.filter_by(username=username).first() is not None:
+                    flash('Please use a different username','danger')
+                    return redirect('/register')
+                else:
+                    user = User(username=username, email=email)
+                    user.set_password(password)
+                    db.session.add(user)
+                    db.session.commit()
+                    flash('Congratulations, you are now a registered user!','success')
+                    return redirect(url_for('login'))
+        else:
+            flash('Fill all the fields','danger')
+            return redirect('/register')
+
+    return render_template('register.html', title='Sign Up page')
+
+@app.route('/record')
+def record():
+    mpDraw = mp.solutions.drawing_utils
+    mpPose = mp.solutions.pose
+    pose = mpPose.Pose()
+ 
+    cap = cv2.VideoCapture('PoseVideos/2.mp4')
+    pTime = 0
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
+        # print(results.pose_landmarks)
+        if results.pose_landmarks:
+            mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+            for id, lm in enumerate(results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+ 
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+ 
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+ 
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+        return render_template('record.html',title='record')
+
+
+@app.route('/forgot',methods=['GET', 'POST'])
+def forgot():
+    if request.method=='POST':
+        email = request.form.get('email')
+        if email:
+            pass
+    return render_template('forgot.html', title='Password reset page')
+    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@login_required
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile.html', user=user, title=f'{user.username} profile')
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/input')
+def input_page():
+    return render_template('input.html',title="Input data")
